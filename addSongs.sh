2c3af6f7bac9b89f27ff2352a0195da3c624f41d
piratepartho <76593389+piratepartho@users.noreply.github.com>
2024-04-14 15:01:29 +0600

    addSongs.sh corrected (uncommented)

--- addSongs.sh
+++ addSongs.sh
@@ -6,12 +6,11 @@ PLAYLIST_ID='insert the playlist id you want to copy to'
 TOTAL_SONGS=100 #add the number of songs you want to add
 
 for ((i = 0; i < $TOTAL_SONGS; i++)); do
-  # uri=$(curl -s --location "https://api.spotify.com/v1/me/tracks?limit=1&offset=$i" \
-  #   --header "Authorization: Bearer $USER_FROM_AUTH" | jq -r '.items[0]["track"]["uri"]')
-  # uri=$(echo $uri | sed -r 's/:/%3A/g')
-  #
-  # response=$(curl -s --location --request POST "https://api.spotify.com/v1/playlists/$PLAYLIST_ID/tracks?uris=$uri" \
-  #   --header "Authorization: Bearer $USER_TO_AUTH")
-  # echo $response
-  echo $i
+  uri=$(curl -s --location "https://api.spotify.com/v1/me/tracks?limit=1&offset=$i" \
+    --header "Authorization: Bearer $USER_FROM_AUTH" | jq -r '.items[0]["track"]["uri"]')
+  uri=$(echo $uri | sed -r 's/:/%3A/g')
+
+  response=$(curl -s --location --request POST "https://api.spotify.com/v1/playlists/$PLAYLIST_ID/tracks?uris=$uri" \
+    --header "Authorization: Bearer $USER_TO_AUTH")
+  echo $response
 done
